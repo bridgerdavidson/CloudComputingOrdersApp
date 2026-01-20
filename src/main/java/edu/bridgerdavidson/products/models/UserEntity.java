5f0802bf39be60a8659512d91136f4a2c58df564
Bridger Davidson <mvbdavidson@gmail.com>
2026-01-20 16:05:30 -0700

    Add UserEntity for authentication and admin management

---
+++ src/main/java/edu/bridgerdavidson/products/models/UserEntity.java
@@ -0,0 +1,72 @@
+package edu.bridgerdavidson.products.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("USERS")
+public class UserEntity {
+    @Id
+    @Column("ID")
+    private int id;
+
+    @Column("USERNAME")
+    private String username;
+
+    @Column("PASSWORD")
+    private String password;
+
+    @Column("ROLE")
+    private Role role;
+
+    @Column("ENABLED")
+    private boolean enabled;
+
+    public UserEntity(int id, String username, String password, Role role, boolean enabled) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.enabled = enabled;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
